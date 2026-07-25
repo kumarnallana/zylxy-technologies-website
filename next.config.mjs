67b459ca6d0e6afff29dabca58c548bc3d64b314
kumar nallana <sasikumarnallana956@gmail.com>
2026-07-25 14:38:47 +0530

    fix(deploy): pivot to custom server architecture

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', // Added to package node_modules directly for Hostinger's production server
   images: {
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
